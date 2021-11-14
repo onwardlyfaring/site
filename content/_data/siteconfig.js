a7017061c84aa278ce5908621b53bab6cfc017db
Matisse Peppet <mcpeppet@Matisses-MBP-2.fios-router.home>
2021-11-14 09:15:00 -0500

    Customize title, sidebar image and text, to be specific to onwardlyfaring; add blackberry image file.

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -2,36 +2,36 @@ require("dotenv").config();
 
 module.exports = {
     // Website title, shown in left sidebar and in page title
-    title: "{ rené.winkelmeyer }",
+    title: "onwardly faring",
     // Site URL to generate absolute URLs. Used across the board.
     url: process.env.URL || "http://localhost:8080",
     // Profile image for left sidebar
-    image: "/assets/images/sidebar_profile.jpg",
+    image: "/assets/images/blackberry5.svg",
     // Image alt text for left sidebar
-    imageAlt: "René and Einstein",
+    imageAlt: "a blackberry",
     // Author name, shown in left sidebar, and used in JSON-LD
-    author: "René Winkelmeyer",
+    author: "matisse peppet",
     // Site description, shown below site image (optional)
-    description: "My kitchen sink about stuff",
+    description: "exploring with heart",
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
-    openGraphDefaultImage: "/assets/images/opengraph.jpg",
-    // GitHub ID (optional, remove it not needed), used for link in the left sidebar
-    socialGitHub: "muenzpraeger",
-    // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
-    socialLinkedIn: "muenzpraeger",
-    // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
-    socialTwitter: "muenzpraeger",
-    // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
-    socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
+    openGraphDefaultImage: "/assets/images/blackberry5.svg",
+    // // GitHub ID (optional, remove it not needed), used for link in the left sidebar
+    // socialGitHub: "muenzpraeger",
+    // // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
+    // socialLinkedIn: "muenzpraeger",
+    // // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
+    // socialTwitter: "muenzpraeger",
+    // // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
+    // socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
-    googleAnalytics: "YOUR_GA-ID",
+    googleAnalytics: "UA-209482536-1",
     // Algolia-powered search  (optional, remove it not needed),
     // See: https://github.com/algolia/algoliasearch-netlify
     algoliaSearch: {
         // When enabled shows the search bar in the UI
-        enabled: true,
+        enabled: false,
         // You'll have to set this manually in your build settings.
         // The value comes from Algolia, and is either visible in the
         // UI for the Crawler Plugin or the Algolia Dashboard.
