bd08ce2f9245940a2729d9b789d07fef7fedcfc7
Matisse Peppet <mcpeppet@matisses-mbp-2.myfiosgateway.com>
2023-02-01 20:56:48 -0500

    Update post css for better dark mode experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
             const extendUnderline = {
                 ".underline": {
                     textDecoration: "underline",
-                    "text-decoration-color": "text-indigo-300",
+                    // "text-decoration-color": "text-indigo-300",
                     "text-underline-position": "under"
                 }
             };
