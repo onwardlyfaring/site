c88dc123d13bbbdc040bba644f5cdd4a405a8a30
Matisse Peppet <mcpeppet@Matisses-MacBook-Pro-2.local>
2023-02-21 22:18:52 -0500

    Remove related-experiment functionality for now -- it was causing trouble with the production build

--- _11ty/experiment-shortcodes.js
+++ _11ty/experiment-shortcodes.js
@@ -36,18 +36,18 @@ const FILTERS = {
             }
         }
         return circString;
-    }, 
-    related: function (collection = []) {
-        const page = this.ctx.page
-        const requiredTags = [page.url]
+    }
+    // , 
+    // related: function (collection = []) {
+    //     const page = this.ctx.page
+    //     const requiredTags = [page.url]
  
-        return collection.filter(post => {
-          // Filter the specified collection, confirm it isn't the current page, and has all the required tags.
-          // Updated to handle potentially missing `tags` properties, per https://github.com/11ty/eleventy/discussions/2534#discussioncomment-3419991 above.
-          return post.url !== page.url && requiredTags?.every(tag => post.data.tags?.includes(tag));
-        });
-      }
-
+    //     return collection.filter(post => {
+    //       // Filter the specified collection, confirm it isn't the current page, and has all the required tags.
+    //       // Updated to handle potentially missing `tags` properties, per https://github.com/11ty/eleventy/discussions/2534#discussioncomment-3419991 above.
+    //       return post.url !== page.url && requiredTags?.every(tag => post.data.tags?.includes(tag));
+    //     });
+    //   }
 }
 const SHORTCODES = {
     intentions: (children) => {
@@ -112,7 +112,7 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPairedShortcode('results', SHORTCODES.results);
     eleventyConfig.addPairedShortcode('contextualintentions', SHORTCODES.contextualintentions);
     eleventyConfig.addFilter("statusCircles", FILTERS.statusCircles);
-    eleventyConfig.addNunjucksFilter("related", FILTERS.related);
+    // eleventyConfig.addNunjucksFilter("related", FILTERS.related);
 
 };
 
