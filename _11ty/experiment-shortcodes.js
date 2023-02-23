1d91003ae6598ef066b621adf5d8186dd3826af5
Matisse Peppet <mcpeppet@Matisses-MacBook-Pro-2.local>
2023-02-23 13:34:39 -0500

    rewrite relatedExperiments filter to not include the ? operator in hopes that it will work in production

--- _11ty/experiment-shortcodes.js
+++ _11ty/experiment-shortcodes.js
@@ -45,7 +45,13 @@ const FILTERS = {
         return collection.filter(post => {
           // Filter the specified collection, confirm it isn't the current page, and has all the required tags.
           // Updated to handle potentially missing `tags` properties, per https://github.com/11ty/eleventy/discussions/2534#discussioncomment-3419991 above.
-          return post.url !== page.url && post.data.tags?.includes(requiredTag)
+          var includedInPostTags;
+          if (post.data.tags === undefined){
+            includedInPostTags = false;
+          } else {
+            includedInPostTags = post.data.tags.includes(requiredTag);
+          }
+          return post.url !== page.url && includedInPostTags;
         });
       }
 }
