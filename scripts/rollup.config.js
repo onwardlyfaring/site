b2976d764ff8a0f2152e86f5539e19b059f3f360
Matisse Peppet <mcpeppet@Matisses-MacBook-Pro-2.local>
2023-10-08 11:04:55 -0400

    move to @rollup/plugin-terser from rollup-plugin-terser@7.0.2 because it had been deprecated

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@rollup/plugin-terser";
 const replace = require("@rollup/plugin-replace");
 const siteconfig = require("../content/_data/siteconfig");
 
